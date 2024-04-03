2bf8d000e15f4f25856c612c08534430f5b0dd53
callum <callumgibbons@icloud.com>
2024-04-03 07:26:01 +0530

    finished initial setup for index html adding all needed buttons. Imported previous card project for skeleton of TS.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,72 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+type Album = {
+  title: string;
+  artist: string;
+  genres: string[];
+  imageSource: string;
+  rating: number;
+  yearOfRelease: number;
+};
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const Albums: Album[] = [
+  {
+    title: "A Moon Shaped Pool",
+    artist: "Radiohead",
+    yearOfRelease: 2016,
+    imageSource:
+      "https://media.pitchfork.com/photos/5929b5b913d197565213ace4/1:1/w_450%2Cc_limit/e3c43867.jpg",
+    rating: 9.1,
+    genres: ["Art Rock", "Ambient", "Post Rock"],
+  },
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  {
+    title: "Amnesiac",
+    artist: "Radiohead",
+    yearOfRelease: 2001,
+    imageSource:
+      "https://media.pitchfork.com/photos/5929a1ee13d197565213874d/1:1/w_450%2Cc_limit/e75d5920.jpeg",
+    rating: 9.0,
+    genres: ["Experimental Rock", "Electronica", "Alternative Rock"],
+  },
+  {
+    title: "Kid A",
+    artist: "Radiohead",
+    yearOfRelease: 2000,
+    imageSource:
+      "https://media.pitchfork.com/photos/5929a1ebb1335d7bf1698393/1:1/w_450%2Cc_limit/dddaf5bb.jpg",
+    rating: 10,
+    genres: ["Experimental Rock", "Art Rock", "Post Rock"],
+  },
+];
+const makeAlbumCard = (album: Album) => {
+  return `<div class="card">
+              <div class="card__header-section">
+                  <img
+                      class="card__image"
+                      src="${album.imageSource}"
+                  />
+                  <div class="card__rating">${album.rating}</div>
+              </div>
+              <div class="card__content">
+                  <h2 class="card__heading">${album.title}</h2>
+                  <div class="card__description">
+                      <p class="card__description-text">${album.artist}</p>
+                      <p class="card__description-text">${
+                        album.yearOfRelease
+                      }</p>
+                  </div>
+                  <hr class="card__line" />
+                  <p class="card__genres">Genres:</p>
+                  <p class="card__genre-values">${album.genres.join(", ")}<p>
+              </div>
+          </div>`;
+};
+
+const container = document.querySelector<HTMLElement>(".card-container");
+
+if (!container) {
+  throw new Error("Issue with selector");
+}
+Albums.forEach((Album) => {
+  container.innerHTML += makeAlbumCard(Album);
+});
