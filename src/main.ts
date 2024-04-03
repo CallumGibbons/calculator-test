e7f17ac774ef1d4eee986e0758107240ed4f758f
callum <callumgibbons@icloud.com>
2024-04-03 11:10:04 +0530

    cleaning up folder and preparing for completion of calc functions.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-const numberButtons = document.querySelectorAll(".number");
-const operatorButtons = document.querySelectorAll(".operator");
-const display = document.querySelector("#display");
-const delAll = document.querySelector("#calcButtonDelAll");
-const del = document.querySelector("#calcButtonDel");
-const equals = document.querySelector("#calcButtonEquals");
-const float = document.querySelector("#calcButtonFloat");
+const numberButtons = document.querySelectorAll(".number")!;
+const operatorButtons = document.querySelectorAll(".operator")!;
+const display = document.querySelector("#display")!;
+const delAll = document.querySelector("#calcButtonDelAll")!;
+const del = document.querySelector("#calcButtonDel")!;
+const equals = document.querySelector("#calcButtonEquals")!;
+const float = document.querySelector("#calcButtonFloat")!;
+
+let displayValue = "0";
+const clearValue = "0";
+
+console.log(del)
