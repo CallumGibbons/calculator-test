7940ad48b7926e9c1e4afd21b7411bf70dc7356a
callum <callumgibbons@icloud.com>
2024-04-03 11:20:38 +0530

    beginning of notes adding to explain each area

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+//all neccesary DOM variables using query selectors to appropriate classes and or IDs
 const numberButtons = document.querySelectorAll(".number")!;
 const operatorButtons = document.querySelectorAll(".operator")!;
 const display = document.querySelector("#display")!;
@@ -5,8 +6,8 @@ const delAll = document.querySelector("#calcButtonDelAll")!;
 const del = document.querySelector("#calcButtonDel")!;
 const equals = document.querySelector("#calcButtonEquals")!;
 const float = document.querySelector("#calcButtonFloat")!;
-
-let displayValue = "0";
-const clearValue = "0";
+//variables needed to reset and initiate display value to be used when calculating later on
+let currentValue = "0";
+const noValue = "0";
 
 console.log(del)
