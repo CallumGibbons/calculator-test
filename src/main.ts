5b1ba4ed91a380c0d23b86dfb27706b5ffbe8dc4
callum <callumgibbons@icloud.com>
2024-04-04 08:27:01 +0530

    Had a breakthrough in my understanding of types. Correct converting of values has enabled for addition and equals to work.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 //all necessary DOM variables using query selectors to appropriate classes and or IDs
 const numberButtons = document.querySelectorAll(".number")!;
-const addButton = document.querySelector("#calcButtonAdd")
+const addButton = document.querySelector("#calcButtonAdd");
 const operatorButtons = document.querySelectorAll(".operator")!;
 const display = document.querySelector("#display")!;
 const delAll = document.querySelector("#calcButtonDelAll")!;
@@ -8,50 +8,60 @@ const del = document.querySelector("#calcButtonDel")!;
 const equals = document.querySelector("#calcButtonEquals")!;
 const float = document.querySelector("#calcButtonFloat")!;
 //variables needed to reset and initiate display value to be used when calculating later on
-let currentValue: string | number = "0";
-let storedValue: number = "0";
-const noValue = "0";
-
+let lastOperator: string = "";
+let currentOperator: string = "";
+let currentValue: number = 0;
+let storedValue: number = 0;
+const noValue = 0;
 
 console.log(del);
 //created the function that will trigger with any button press, and display whatever is inside the html for the user to see what is happening whilst also storing it to the current value function
 const buttonClick: any = (event: typeof buttonClick) => {
-  if (currentValue == "0") {
+  if (currentValue == 0) {
+    display.innerHTML = "";
     display.innerHTML += event.target.innerText;
-    noValue = display.innerHTML;
+    currentValue = parseFloat(display.innerHTML);
   } else {
     display.innerHTML += event.target.innerText;
-    currentValue = display.innerHTML;
+    currentValue = parseFloat(display.innerHTML);
   }
 };
 
 const addClicked = () => {
-  if (sum(currentValue + storedValue) == "0" {
-
-  }
-}
-
-const equalsClicked = () =>{
-  if (currentValue == "0") {
-    display.innerHTML = currentValue
-  } else {
-    storedValue = currentValue
-    display.innerHTML = currentValue
-  }
-}
-const delClicked = () => {
-  if (currentValue == "0") {
-    currentValue = display.innerHTML;
-  } else {
-    display.innerHTML = storedValue;
-  }
-}
-
-const delAllClicked = () => {
-  currentValue = "0";
-  return (display.innerHTML = noValue);
+  lastOperator = currentOperator;
+  currentOperator = "+";
+  storedValue = currentValue;
+  currentValue = noValue;
+  display.innerHTML = currentValue.toString();
 };
 
+const equalsClicked = () => {
+  if (currentValue == 0) {
+    display.innerHTML = currentValue.toString();
+  } else if (currentOperator == "+") {
+    storedValue = currentValue + storedValue;
+    console.log(storedValue);
+    currentValue = noValue;
+    currentOperator = "";
+    display.innerHTML = storedValue.toString();
+    console.log(currentValue);
+  } else {
+    storedValue = currentValue;
+    display.innerHTML = storedValue.toString();
+  }
+};
+const delClicked = () => {
+  if (currentValue == 0) {
+    currentValue = parseFloat(display.innerHTML);
+  } else {
+    display.innerHTML = storedValue.toString();
+  }
+};
+
+const delAllClicked = () => {
+  currentValue = 0;
+  return (display.innerHTML = noValue.toString());
+};
 
 //added event listeners below for each button type, to be used above in functions as events
 numberButtons.forEach((numberButton) => {
@@ -63,4 +73,6 @@ operatorButtons.forEach((operatorButton) => {
 
 delAll.addEventListener("click", delAllClicked);
 
-equals.addEventListener("click", equalsClicked)
+equals.addEventListener("click", equalsClicked);
+
+addButton.addEventListener("click", addClicked);
